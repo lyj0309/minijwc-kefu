94e7c65e09c3364720b2f8153d81a6a00e65dfe8
lyj <2457395722@qq.com>
2022-01-22 18:33:00 +0800

    :speech_balloon: 更新回复信息

---
+++ model/text.go
@@ -0,0 +1,27 @@
+package model
+
+const (
+	NoAnswer = `小掌没有明白您的意思，要不换个问法试试？您可参考以下方式提问，或者回复【转人工】进一步咨询~
+
+什么时候选课
+如何看下学期课表
+这里能够看到专业排名吗
+`
+	OffNoAnswer = `小掌没有明白您的意思，要不换个问法试试？您可参考以下方式提问，或者前往小程序-设置-联系客服，回复“人工”进一步咨询~
+
+
+什么时候选课
+如何看下学期课表
+这里能够看到专业排名吗
+`
+	Hello = `"Hi~，欢迎咨询掌上教务处，人工智障客服小掌为您排忧解惑，请问有什么可以为您效劳呢?"`
+
+	OffHello = `欢迎关注掌上教务处，等你很久喽
+
+<a data-miniprogram-appid="wxc8d375bbcf2ac52f" 
+data-miniprogram-path="pages/Subpages/StudentId/StudentId" 
+href="https://www.qq.com">点这里,登录教务处账号</a>，即可获取课程表查看成绩等服务
+
+有任何问题直接回复即可
+`
+)
