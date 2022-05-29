067aae649272dffe588c24ef7eee29df80a16014
lyj0309 <lyj@fakev.cn>
2022-05-29 14:59:28 +0800

    🎨

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	r.GET("/official", func(c *gin.Context) { c.String(200, c.Query("echostr")) })
 	r.POST("/", model.Kefu)               //客服
 	r.POST("/official", model.WxOfficial) //公众号
-	err := r.Run(":8887")
+	err := r.Run(":9000")
 	if err != nil {
 		return
 	}
